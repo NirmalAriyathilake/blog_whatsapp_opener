4f7cd1331a8a738bdc27f4e9cd9220dc37d6a022
Nirmal Ariyathilake <nirmal.ariyathilake@gmail.com>
2023-02-05 07:12:29 +0530

    Initial home page ui

--- lib/presentation/home_page.dart
+++ lib/presentation/home_page.dart
@@ -5,8 +5,29 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      appBar: AppBar(),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Whatsapp Opener"),
+        centerTitle: true,
+      ),
+      body: Container(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 10,
+          vertical: 5,
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            const TextField(
+              decoration: InputDecoration(labelText: 'Enter number'),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text("Open Whatsapp"),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
